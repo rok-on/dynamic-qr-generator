05ffb2f2e54b108050652b0f52257861fdd27d27
rok-on <84695130+rok-on@users.noreply.github.com>
2025-10-17 14:25:38 +0200

    Update Notification.tsx

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from 'react';
-import type { NotificationType } from '../types';
+import type { NotificationType } from '../types/index.ts';
 import { CheckCircle, XCircle, X } from 'lucide-react';
 
 interface NotificationProps {
