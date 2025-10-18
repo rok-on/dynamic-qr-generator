76ff97efd0feba3a2f0486e9885e77cce970b156
rok-on <84695130+rok-on@users.noreply.github.com>
2025-10-18 11:40:03 +0200

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface QRImageSettings {␊
+  src: string; // base64 data URL␊
+  height: number;␊
+  width: number;␊
+  excavate: boolean;␊
+}␊
+␊
+export interface QROptions {␊
+  fgColor: string;␊
+  bgColor: string;␊
+  level: 'L' | 'M' | 'Q' | 'H';␊
+  imageSettings?: QRImageSettings | null;␊
+}␊
+␊
+export interface Link {␊
+  id: string;␊
+  destinationUrl: string;␊
+  shortUrl: string;␊
+  createdAt: number;␊
+  scanCount?: number;
+  updatedAt?: number;
+  qrOptions?: QROptions;␊
+}␊
+␊
+export type NotificationType = 'success' | 'error';
